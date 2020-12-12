02012e242076884097fc71624ee1598b70251743
UniquelyElite <rylanmetznerm@gmail.com>
2020-12-12 23:56:41 +0000

    modifed some stuff, idk why its not working still-

--- Day6/task2/index.py
+++ Day6/task2/index.py
@@ -5,22 +5,22 @@ lines = text.split("\n")
 
 responses = []
 chosen = 0
-found = 0
-responseAmount = 0
+firstLine = True
+line = 0
+
 for x in lines :
-    if x != '' :
-        responseAmount += 1
-        for y in x :
-            if not y in responses :
-                
+    line += 1
+    if x != '' and not line >= len(lines) :
+        if firstLine :
+            firstLine = False
+            for y in x :
                 responses.append(y)
-                found += 1
-            else :
-                found += 1
+        else :
+            for y in responses :
+                if not y in x :
+                    responses.remove(y)
     else :
+        firstLine = True
+        chosen += len(responses)
         responses = []
-        print(found//responseAmount)
-        chosen += found//responseAmount
-        found = 0
-        responseAmount = 0
 print(chosen, 'group "yes" responses')
